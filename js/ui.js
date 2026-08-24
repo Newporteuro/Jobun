5a64e678b7d0ce92e8ff4303d12425404b46482e
Newporteuro <kenkichiro@gmail.com>
2026-08-24 09:21:35 +0900

    Add files via upload

--- js/ui.js
+++ js/ui.js
@@ -7,18 +7,18 @@
 /* すべての import に同じ ?v= を付ける。GitHub Pages は max-age=600 を返すため、
    これが無いと index.html だけ新しく、モジュールは古いままという状態が10分間続く。
    ファイルを更新したら VERSION と各 import の ?v= を必ず揃えて上げ直すこと。 */
-export const VERSION = "20260824a";
+export const VERSION = "20260824b";
 
-import { LAWS, SCOPES, weightOf } from "./weights.js?v=20260824a";
+import { LAWS, SCOPES, weightOf } from "./weights.js?v=20260824b";
 import {
   fetchArticle, fetchIndex, renderArticle, fullText,
   fetchWikitext, parsePrecedents, parseDoctrines, wikiURL,
-} from "./sources.js?v=20260824a";
+} from "./sources.js?v=20260824b";
 import {
   makeBlank, makeDescriptive, makeDoctrine,
   isPoorQuestion, similarity, scoreCase, weightedPick, pick,
-} from "./drill.js?v=20260824a";
-import { CASES } from "./cases.js?v=20260824a";
+} from "./drill.js?v=20260824b";
+import { CASES } from "./cases.js?v=20260824b";
 
 const $ = s => document.querySelector(s);
 const esc = s => s.replace(/[&<>]/g, c => ({"&":"&amp;","<":"&lt;",">":"&gt;"}[c]));
