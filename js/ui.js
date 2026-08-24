b13937cf0482f67791b16dbea67c07a94244cd6a
Newporteuro <kenkichiro@hotmail.com>
2026-08-24 21:02:57 +0900

    minpo-1028 で、居住の書き方の幅を広げる
    「Aの死亡以前から甲建物に居住しており」と書いた答案が、相続開始時の居住
    （7点）を落としていた。キーワードが「死亡の時に居住」の形しか見ておらず、
    「死亡以前から居住」「生前から同居」を拾えていなかった。いずれも相続開始時に
    住んでいたことを述べている。
    
    「死亡後に移り住んだ」は要件を満たさないので、前／以前／生前に限る。
    
    selftest.html にプローブを3件追加（合計59件）。版を 20260824i に。

--- js/ui.js
+++ js/ui.js
@@ -7,18 +7,18 @@
 /* すべての import に同じ ?v= を付ける。GitHub Pages は max-age=600 を返すため、
    これが無いと index.html だけ新しく、モジュールは古いままという状態が10分間続く。
    ファイルを更新したら VERSION と各 import の ?v= を必ず揃えて上げ直すこと。 */
-export const VERSION = "20260824h";
+export const VERSION = "20260824i";
 
-import { LAWS, SCOPES, weightOf } from "./weights.js?v=20260824h";
+import { LAWS, SCOPES, weightOf } from "./weights.js?v=20260824i";
 import {
   fetchArticle, fetchIndex, renderArticle, fullText,
   fetchWikitext, parsePrecedents, parseDoctrines, wikiURL,
-} from "./sources.js?v=20260824h";
+} from "./sources.js?v=20260824i";
 import {
   makeBlank, makeDescriptive, makeDoctrine,
   isPoorQuestion, similarity, scoreCase, weightedPick, pick,
-} from "./drill.js?v=20260824h";
-import { CASES } from "./cases.js?v=20260824h";
+} from "./drill.js?v=20260824i";
+import { CASES } from "./cases.js?v=20260824i";
 
 const $ = s => document.querySelector(s);
 const esc = s => s.replace(/[&<>]/g, c => ({"&":"&amp;","<":"&lt;",">":"&gt;"}[c]));
